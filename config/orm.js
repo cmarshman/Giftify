bd8d7f9a99a4addd193afd467715573a586c94c1
Greg Roberts <groberts3104@gmail.com>
2020-01-30 08:59:24 -0500

    removed unwanted comments

--- config/orm.js
+++ config/orm.js
@@ -21,12 +21,9 @@ function printQuestionMarks(num) {
       var value = ob[key];
       // check to skip hidden properties
       if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
         if (typeof value === "string" && value.indexOf(" ") >= 0) {
           value = "'" + value + "'";
         }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
         arr.push(key + "=" + value);
       }
     }
