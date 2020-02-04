e433ae2737ff0d0fc4ebfa3f8b11fbe69426df93
Greg Roberts <groberts3104@gmail.com>
2020-02-04 18:34:19 -0500

    added function for search button to append buttons of search items

--- public/js/script.js
+++ public/js/script.js
@@ -10,16 +10,37 @@ $('a[href^="#"]').on('click', function (event) {
     }
 });
 
-$(searchBtnEl).click(function(response) {
+$(searchBtnEl).click(function () {
     var inputVal = $("#inputInfo").val();
-    console.log(!inputVal);
+    var giftBtn = document.createElement("button");
+    
+    giftBtn.innerHTML = inputVal;
+    document.getElementById("giftVal").prepend(giftBtn);
 
-    if (!inputVal){
+    console.log(inputVal);
+
+    var newGift = {
+      gift_name: $("#inputInfo").val(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/gifts", {
+      type: "POST",
+      data: newGift
+    }).then(
+      function() {
+        console.log("created new gift");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+
+    if (!inputVal) {
         return;
         console.log('input is null')
     }
     else {
         event.preventDefault();
-        console.log(inputVal)
+        console.log(inputVal + " has been entered.")
     }
-})
+});
