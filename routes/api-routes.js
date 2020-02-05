6306196f87e0cc8992fbaf8b43bce7aead2ea229
Greg Roberts <groberts3104@gmail.com>
2020-02-04 20:28:29 -0500

    added gift-handlebars in partials folder

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,38 +2,38 @@
 var db = require("../models");
 var passport = require("../config/passport");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/signup", function (req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
-      .then(function() {
+      .then(function () {
         res.redirect(307, "/api/login");
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.status(401).json(err);
       });
   });
 
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
+  app.get("/api/user_data", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -46,28 +46,25 @@ module.exports = function(app) {
       });
     }
   });
-};
-
-module.exports = function(app) {
 
   // GET route for getting all of the todos
-  app.get("/api/gifts", function(req, res) {
+  app.get("/api/gifts", function (req, res) {
     // findAll returns all entries for a table when used with no options
-    db.GiftList.findAll({}).then(function(result) {
+    db.GiftList.findAll({}).then(function (result) {
       // We have access to the giftsList as an argument inside of the callback function
       res.json(result);
     });
   });
 
   // POST route for saving a new todo
-  app.post("/api/gifts", function(req, res) {
+  app.post("/api/gifts", function (req, res) {
     console.log(req.body);
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
     db.GiftList.create({
       gift_name: req.body.gift_name
-    }).then(function(result) {
+    }).then(function (result) {
       // We have access to the new giftlist as an argument inside of the callback function
       res.json(result);
     });
@@ -75,15 +72,15 @@ module.exports = function(app) {
 
   // DELETE route for deleting gifts. We can get the id of the gift we want to delete from
   // req.params.id
-  app.delete("/api/gifts/:id", function(req, res) {
-        // We just have to specify which gift we want to destroy with "where"
-        db.GiftsList.destroy({
-          where: {
-            id: req.params.id
-          }
-        }).then(function(result) {
-          res.json(result);
-        });
-    
+  app.delete("/api/gifts/:id", function (req, res) {
+    // We just have to specify which gift we want to destroy with "where"
+    db.GiftsList.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (result) {
+      res.json(result);
+    });
+
   });
 };
