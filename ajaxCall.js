5c4b3f073bf0a51625cb90413d66c5604b1582d1
JT Turner <j.tturner14@yahoo.com>
2020-02-06 18:58:21 -0500

    etsy api call, displays images

--- ajaxCall.js
+++ ajaxCall.js
@@ -3,7 +3,7 @@ const searchBtn = document.querySelector('#gift_search')
 
 
 function getDataFromEtsyApi(searchTerm, callback) {
-    var ETSY_BASE_URL = 'https://openapi.etsy.com/v2/listings/active.js';
+    var ETSY_BASE_URL = 'https://openapi.etsy.com/v2/listings/active.js?limit=10';
     var ETSY_KEY = 'sg1y7p6kuosq2seimizzx5ml';
     
     var settings = {
@@ -20,20 +20,55 @@ function getDataFromEtsyApi(searchTerm, callback) {
     
     $.ajax(settings);
 };  
-//Show Esty results
+//Show Esty results and loop through response object
 function displayEtsySearchData(data) {
-    // for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < data.results.length; i++) {
 
     //  $('.subtitle').append(data.results[i].title)
 
     // }
-    // // console.log(data.results[i]);
-    // // console.log(data.results[i].price);
+    // console.log(data.results[i]);
+    // console.log(data.results[i].price);
+    // console.log(data.results[i].title);
+    // console.log(data.results[1].url);
+    // console.log(data.results[1].price);
+    
+    //displaying title as a link, opens in another tab
+    // var titleDisplay = $("<a>").text(data.results[i].title).attr("href", data.results[i].url).attr("target", "blank");
+    // $("#showResults").prepend(titleDisplay);
+
+    var titleDisplay = $("<div>").append($("<a>").attr("href", data.results[i].url).text(data.results[i].title).css("display", "flex").css("align-items", "center").css("word-wrap", "break-word").attr("target", "_blank"));
+
+    var pictureDisplay = $("<div>").append($("<img>").attr("src", data.results[i].Images[0].url_fullxfull).attr("id", "pictures").css("width", "96px").css("clear", "both").css("margin-bottom", "10px").css("display", "inline-block").css("margin-right", "10px").css("@media screen and (max-width: 600px)", "{body {background-color: olive;}}"));
+
+    var newDiv = $("<div>").attr("id", "resultBlock" + i).css("display", "flex").css("padding", "1px");
+
+    $("#showResults").append(newDiv);
+    $(newDiv).append(pictureDisplay, titleDisplay);
+
+    // var imageDisplay = $("<div>").append($("<img>").attr("scr", data.results[1].Images[1].url_75x75).attr("id", "pictures").css("width", "96px"));
+    // console.log(imageDisplay);
+
+    // var newDiv = $("<div>");
+
+    // $(newDiv).append(imageDisplay);
+    // var picture = $("<img>").attr("src", data.results[i].Images[0].url_fullxfull);
+
+    // $("pictures").append(picture);
+    
+    
+
+    
+    // document.getElementById('showResults').text(data.results[1].title);
+
+    // titleDisplay = document.getElementById('inputInfo').value;
+    // console.log(titleDisplay);
+
 
 
     console.log(data);
 
-};
+}};
 
 
 if (searchBtn) {
